088bff81c9ba8c80ea91d32d7cb2f0d816740c2d
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2024-07-07 10:30:06 +0530

    Time: 79 ms (14.29%) | Memory: 100.9 MB (14.29%) - LeetSync

---
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+
+        int ans=0;
+        int maxLen=1;
+        int n = colors.size();
+        for(int i=0;i<n-1+k-1;i++){
+            if(colors[i%n]!=colors[(i-1+n)%n]){
+                maxLen++;
+            }else maxLen=1;
+
+            if(maxLen >= k) ans++;
+        }
+        return ans;
+    }
+};
