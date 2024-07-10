5fcd33fd515813732f3130f6047552d6389b377f
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2024-07-10 20:19:44 +0530

    Time: 4 ms (67.13%) | Memory: 13.9 MB (61.11%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int folderDepth = 0;
+
+     
+        for (const auto& currentOperation : logs) {
+          
+            if (currentOperation == "../") {
+                folderDepth = max(0, folderDepth - 1);
+            }
+          
+            else if (currentOperation != "./") {
+               
+                ++folderDepth;
+            }
+         
+        }
+
+        return folderDepth;
+    }
+};
