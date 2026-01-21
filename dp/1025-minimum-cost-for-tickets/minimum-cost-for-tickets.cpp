5ed678c0d36ceae3129238f66f5869cc01b69cfb
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-01-21 22:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.57%) - LeetSync

--- dp/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ dp/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& days, vector<int>& costs,vector<int>&dp) {
-        if (index >= days.size()) return 0;
+    int dp[401];
+    int solve(vector<int>& days, vector<int>& costs,int index , int maxDay){
+        if(index >= days.size()) return 0;
+        
+        if(maxDay >= days[index]) return solve(days,costs,index+1,maxDay);
         if(dp[index]!=-1) return dp[index];
-        int i = index;
-        int cost1 = costs[0] + solve(i+1,days,costs,dp);
-        while(i<days.size() && days[i] < days[index]+7) i++;
-        int cost2 = costs[1] + solve(i,days,costs,dp);
-
-        i = index;
-        while (i < days.size() && days[i] < days[index] + 30) i++;
-        int cost3 = costs[2] + solve(i, days, costs, dp);
-
-
-
-       return dp[index] = min({cost3,cost2,cost1});
-       
+        //day1 pass
+        int ans = costs[0] + solve(days,costs,index+1,days[index]);
+        //day2 pass 
+        ans = min(ans,costs[1] + solve(days,costs,index+1,days[index]+6 ));
+        //day3 pass
+        ans = min(ans,costs[2] + solve(days,costs,index+1,days[index]+29));
+    
+        return dp[index] = ans;
     }
-
-
-
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-            int n = days.size();
-    vector<int> dp(n, -1);
-        return solve(0, days, costs,dp);
+        memset(dp,-1,sizeof(dp));
+        return solve(days,costs,0,0);
     }
-
-
-
-    int mincostTickets1(vector<int>& days, vector<int>& costs) {
-    unordered_set<int> travel(days.begin(), days.end());
-    int lastDay = days.back();
-    int dp[366] = {0};
-
-    for (int d = 1; d <= lastDay; d++) {
-        if (travel.count(d) == 0) {
-            dp[d] = dp[d - 1]; // no travel today
-        } else {
-            dp[d] = min({
-                dp[max(0, d - 1)] + costs[0],
-                dp[max(0, d - 7)] + costs[1],
-                dp[max(0, d - 30)] + costs[2]
-            });
-        }
-    }
-
-    return dp[lastDay];
-}
 };
