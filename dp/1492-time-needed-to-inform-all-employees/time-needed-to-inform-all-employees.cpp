7982c26426920e833d42f110b7b47dca1e40f1ba
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-03-26 17:34:14 +0530

    Time: 230 ms (24.09%) | Memory: 161.7 MB (18.42%) - LeetSync

--- dp/1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ dp/1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int dfs(int node, unordered_map<int, vector<int>>& mp, vector<int>& informTime) {
-        int maxTime = 0;
-
-        for (auto child : mp[node]) {
-            maxTime = max(maxTime, dfs(child, mp, informTime));
-        }
-
-        return informTime[node] + maxTime;
+    int solve(int node, vector<int>& it,unordered_map<int,vector<int>>&mp){
+      int maxTime =0;
+      for(auto sub: mp[node] ){
+        maxTime=max(solve(sub,it,mp) , maxTime);
+      }
+      return maxTime + it[node];
     }
-
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int, vector<int>> mp;
-
-        // build tree
-        for (int i = 0; i < n; i++) {
+        unordered_map<int,vector<int>>mp;
+          for (int i = 0; i < n; i++) {
             if (manager[i] != -1) {
                 mp[manager[i]].push_back(i);
             }
         }
-
-        return dfs(headID, mp, informTime);
+        return solve(headID,informTime,mp);
+       
     }
 };
