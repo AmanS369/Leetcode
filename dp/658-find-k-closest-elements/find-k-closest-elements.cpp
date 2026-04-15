8c428e2a4b88032c882451ce6d31d57f8eb33ed9
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-04-15 21:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 36 MB (64.79%) - LeetSync

--- dp/658-find-k-closest-elements/find-k-closest-elements.cpp
+++ dp/658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -16,35 +16,55 @@ public:
         ans.push_back(arr[i]);
         i--;
         j++;
-        
+        int t=k;
         k--;
         while(k > 0){
     
 
             if(i>=0 && j<n){
                 if(abs(x-arr[i]) > abs(x-arr[j])){
-                 ans.push_back(arr[j]);
+              
                   j++;
                 }else{
-                    ans.push_back(arr[i]);
+                  
                     i--;
                 }
             }else{
                 if(i>=0){
-                  ans.push_back(arr[i]);
                   i--;
                 }
 
                 else if(j<n){
-                    ans.push_back(arr[j]);
                     j++;
                 }
             }
             k--;
         }
 
-        sort(ans.begin(),ans.end());
+    return vector<int>(arr.begin() + i + 1, arr.begin() + i + 1 + t );
 
-        return ans;
     }
+
+
+    
 };
+
+
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         int left = 0, right = arr.size() - k;
+
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+
+//             if (x - arr[mid] > arr[mid + k] - x) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid;
+//             }
+//         }
+
+//         return vector<int>(arr.begin() + left, arr.begin() + left + k);
+//     }
+// };
