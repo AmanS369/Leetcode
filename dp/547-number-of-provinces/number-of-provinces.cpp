5ef75181bbdedadef7ac519c8315c95771c208a1
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-05-30 19:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (72.83%) - LeetSync

---
+++ dp/547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,62 @@
+class DisjointSet {
+public:
+    vector<int> parent, size;
+
+    DisjointSet(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+
+        if (pu == pv)
+            return;
+
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        DisjointSet ds(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    ds.unionBySize(i, j);
+                }
+            }
+        }
+
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (ds.findUPar(i) == i)
+                provinces++;
+        }
+
+        return provinces;
+    }
+};
