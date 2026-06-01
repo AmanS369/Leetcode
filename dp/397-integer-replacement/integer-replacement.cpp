fd911bcacbd4f07688442d6e50ff965bb7526e8d
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-06-01 08:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (19.06%) - LeetSync

---
+++ dp/397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_map<long long, int> dp;
+
+    int solve(long long n) {
+        if (n == 1) return 0;
+
+        if (dp.count(n))
+            return dp[n];
+
+        if (n % 2 == 0)
+            return dp[n] = 1 + solve(n / 2);
+
+        return dp[n] = 1 + min(solve(n - 1), solve(n + 1));
+    }
+
+    int integerReplacement(int n) {
+        return solve((long long)n);
+    }
+};
