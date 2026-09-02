be90b379523f144e93feea83f35973b93a5071f1
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2026-09-02 07:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (94.95%) - LeetSync

---
+++ dp/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n = nums1.size();
+
+        // int oddC = 0;
+        // int evenC = 0;
+
+        // for (int x : nums1) {
+        //     if (x % 2 == 0)
+        //         evenC++;
+        //     else
+        //         oddC++;
+        // }
+
+        // if (evenC == n  || oddC == n  || evenC == oddC)
+        //     return true;
+
+        // if (evenC > oddC && oddC > 1)
+        //     return true;
+
+        // if (evenC < oddC && evenC > 1)
+        //     return true;
+
+        return true;
+    }
+};
